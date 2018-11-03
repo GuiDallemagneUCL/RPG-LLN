544c34a3fc3e245881e13335c1eb6f59ea1e5f4a
AlexandreFiset001 <alexandre.fiset@student.uclouvain.be>
2018-11-03 23:04:45 +0100

    add background sound

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pygame
 import pygame.locals
 from Level import Level
- 
+
 
 
 # define a main function
@@ -21,10 +21,25 @@ def main():
 	screen.blit(background,view_coord)
 	pygame.display.flip()
 
+    #load button and getting his collide zone
+	bouton = pygame.image.load("images/sound_icon.png")
+	bouton_rect = bouton.get_rect()
+
+	#sound played
+	sound_played = True
+
+	#load music
+	pygame.mixer.init()
+	pygame.mixer.music.load("sound/lln_sound.wav")
+	pygame.mixer.music.play(-1,0.0)
+
 	while running:
 		screen.blit(background,view_coord)
+		bouton = pygame.transform.scale(bouton, (32, 32))
+		screen.blit(bouton, (0,0))
 		pygame.display.flip()
 		clock.tick(15)
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
@@ -37,6 +52,24 @@ def main():
 					view_coord = (view_coord[0] + screen.get_rect().width / grid_width, view_coord[1])
 				elif event.key == pygame.locals.K_RIGHT:
 					view_coord = (view_coord[0] - screen.get_rect().width / grid_width, view_coord[1])
+			## Si le focus est sur la fenêtre.
+			if pygame.mouse.get_focused():
+				## Trouve position de la souris
+				x, y = pygame.mouse.get_pos()
+				## S'il y a collision:
+				collide = bouton_rect.collidepoint(x, y)
+
+				## Détecte les clique de souris.
+				pressed = pygame.mouse.get_pressed()
+				if pressed[0] and collide: # 0=gauche, 1=milieu, 2=droite
+					if sound_played:
+						bouton = pygame.image.load("images/no_sound_icon.png")
+						pygame.mixer.music.stop()
+						sound_played = False
+					else:
+						bouton = pygame.image.load("images/sound_icon.png")
+						pygame.mixer.music.play(-1,0.0)
+						sound_played = True
 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
